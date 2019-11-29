290226b2e8c626e9e11f4242062012c5f6c9d321
Jure <jurejankovic@gmail.com>
2019-11-29 10:06:00 +0100

    Some fixes

--- app/src/main/java/hr/vsite/dipl/quicktimetable/LocationService.java
+++ app/src/main/java/hr/vsite/dipl/quicktimetable/LocationService.java
@@ -3,12 +3,16 @@ package hr.vsite.dipl.quicktimetable;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Looper;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.widget.Toast;
 
@@ -43,9 +47,21 @@ public class LocationService extends Service {
         criteria.setAccuracy(Criteria.ACCURACY_COARSE);
         criteria.setPowerRequirement(Criteria.POWER_LOW);
         String bestProvider = locationManager.getBestProvider(criteria, true);
-        locationManager.requestLocationUpdates(bestProvider, 0, 0, locationListener);
-        //locationManager.requestSingleUpdate(bestProvider, locationListener, );
 
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (bestProvider == null){
+                //locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                //locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                //locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            }
+            else {
+                locationManager.requestLocationUpdates(bestProvider, 0, 0, locationListener);
+                //locationManager.requestSingleUpdate(bestProvider, locationListener, Looper.myLooper());
+            }
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
